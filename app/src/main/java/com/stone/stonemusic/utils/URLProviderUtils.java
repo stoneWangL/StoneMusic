8e7785fae9acd24c1d71319e4a5ca98702549fdf
stoneWang <stoneWang_L@163.com>
2019-08-30 23:33:30 +0800

    upData: 加入查询接口 Result：
    Next：
    2、编写相关的接口处理类
    
    1、查询界面和功能的设置
    
    0.实现拖拽（现在将显示的做好，时间紧迫，歌词拖拽往后延）

--- app/src/main/java/com/stone/stonemusic/utils/URLProviderUtils.java
+++ app/src/main/java/com/stone/stonemusic/utils/URLProviderUtils.java
@@ -1,84 +1,8 @@
 package com.stone.stonemusic.utils;
 
-import android.util.Log;
+
 
 public class URLProviderUtils {
-
-    /**
-     * 获取首页的url
-     *
-     * @param offset 数据偏移量
-     * @param size   返回数据的条目个数
-     * @return url
-     */
-    public static String getHomeUrl(int offset, int size) {
-        String url = "http://mapi.yinyuetai.com/suggestions/front_page.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&offset=" + offset
-                + "&size=" + size
-                + "&v=4&rn=640*540";
-        Log.i("Main_url", url);
-        return url;
-    }
-
-    public static String getMVareaUrl() {
-        String url = "http://mapi.yinyuetai.com/video/get_mv_areas.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}";
-        return url;
-    }
-
-    public static String getMVListUrl(String area, int offset, int size) {
-        String url = "http://mapi.yinyuetai.com/video/list.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&area=" + area
-                + "&offset=" + offset
-                + "&size=" + size;
-        return url;
-    }
-
-    public static String getYueDanUrl(int offset, int size) {
-        String url = "http://mapi.yinyuetai.com/playlist/list.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&offset=" + offset
-                + "&size=" + size;
-        return url;
-    }
 
     //网友推荐歌单-分类 http://144.34.228.215:3000/top/playlist/catlist?limit=2&offset=0&cat=怀旧
     public static String getRecommendAll(int offset, int size, String cat) {
@@ -126,148 +50,22 @@ public class URLProviderUtils {
         return url;
     }
 
-    /**
-     * <p>获取音乐节目列表</p>
-     *
-     * @param artistIds
-     * @param offset
-     * @param size
-     * @return
-     */
-    public static String getYinYueProgramList(String artistIds, int offset, int size) {
-        String url = "http://mapi.yinyuetai.com/playlist/show.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&offset=" + offset
-                + "&size=" + size
-                + "&artistIds=" + artistIds;
-        return url;
-    }
 
     /**
-     * 获取V榜地址
-     *
-     * @return
+     * 关键字搜索
+     * @param keyWord 关键词
+     * @param type 搜索类型；默认为 1 即单曲 , 取值意义 : 1: 单曲, 10: 专辑, 100: 歌手, 1000: 歌单, 1002: 用户, 1004: MV, 1006: 歌词, 1009: 电台, 1014: 视频
+     * @param limit 返回数量 , 默认为 30
+     * @param offset 偏移数量，用于分页
+     * @return String 类型的JSON数据
      */
-    public static String getVChartAreasUrl() {
-        String url = "http://mapi.yinyuetai.com/vchart/get_vchart_areas.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}";
+    public static String findByKeyWord(String keyWord, int type, int limit, int offset) {
+        String url = "http://144.34.228.215:3000/search/multimatch?keywords="
+                + keyWord
+                + "&type=" + type
+                + "&limit=" + limit
+                + "&offset=" + offset;
         return url;
-    }
-
-    /**
-     * 获取V榜的周期
-     *
-     * @return
-     */
-    public static String getVChartPeriodUrl(String area) {
-        String url = "http://mapi.yinyuetai.com/vchart/period.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&area=" + area;
-        return url;
-    }
-
-    /**
-     * 获取V榜列表
-     *
-     * @param area
-     * @param dateCode
-     * @return
-     */
-    public static String getVChartListUrl(String area, int dateCode) {
-        String url = "http://mapi.yinyuetai.com/vchart/show.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&area=" + area
-                + "&datecode=" + dateCode;
-        return url;
-    }
-
-    /**
-     * 获取相关MV
-     *
-     * @param id
-     * @return
-     */
-    public static String getRelativeVideoListUrl(int id) {
-        String url = "http://mapi.yinyuetai.com/video/show.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&relatedVideos=true"
-                + "&id=" + id;
-        return url;
-    }
-
-    /**
-     * 通过id 获取某人的悦单
-     *
-     * @param id
-     * @return
-     */
-    public static String getPeopleYueDanList(int id) {
-        String url = "http://mapi.yinyuetai.com/playlist/show.json?deviceinfo="
-                + "{\"aid\":\"10201036\",\"os\":\"Android\","
-                + "\"ov\":" + "\"" + getSystemVersion() + "\"" + ","
-                + "\"rn\":\"480*800\","
-                + "\"dn\":" + "\"" + getPhoneModel() + "\"" + ","
-                + "\"cr\":\"46000\","
-                + "\"as\":"
-                + "\"WIFI\","
-                + "\"uid\":"
-                + "\"dbcaa6c4482bc05ecb0bf39dabf207d2\","
-                + "\"clid\":110025000}"
-                + "&id=" + id;
-        return url;
-    }
-
-    private static String getSystemVersion() {
-        return android.os.Build.VERSION.RELEASE;
-    }
-
-    private static String getPhoneModel() {
-        return android.os.Build.MODEL;
     }
 }
 
